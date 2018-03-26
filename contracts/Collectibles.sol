bbe4910a3d05970e15c9cf1bd81b67dcde95a2c7
Mihai Cimpoesu <mihai.cimpoesu@gmail.com>
2018-03-26 21:50:00 +0100

    Adding metadata functionality

--- contracts/Collectibles.sol
+++ contracts/Collectibles.sol
@@ -7,6 +7,14 @@ contract Collectibles is Ownable, ERC721Token {
 
 	address public adaptAdmin;
 
+	struct TokenMetadata {
+		uint32 timestamp;
+		uint amount;
+	}
+
+	// Optional mapping for token metadata
+	mapping(uint256 => TokenMetadata) internal metadata;
+
 	function Collectibles(
 		address _adaptOwner,
 		address _adaptAdmin) public
@@ -21,15 +29,27 @@ contract Collectibles is Ownable, ERC721Token {
 		_;
 	}
 
-	function mint(address _to, uint256 _tokenId) onlyAdmin public {
+	function mint(address _to, uint256 _tokenId) public onlyAdmin {
 		super._mint(_to, _tokenId);
 	}
 
-	function setTokenURI(uint256 _tokenId, string _uri) onlyAdmin public {
+	function setTokenURI(uint256 _tokenId, string _uri) public onlyAdmin {
 		super._setTokenURI(_tokenId, _uri);
 	}
 
 	function changeAdmin(address _newAdmin) public onlyOwner {
 		adaptAdmin = _newAdmin;
+	}
+
+	function setTokenMetadata(uint256 _tokenId, uint32 _timestamp, uint _amount) public canTransfer(_tokenId)  {
+		TokenMetadata storage tm = metadata[_tokenId];
+		require(tm.timestamp == 0 && tm.amount == 0);
+		metadata[_tokenId] = TokenMetadata({timestamp : _timestamp, amount: _amount});
+	}
+
+	function getTokenMetadata(uint256 _tokenId) public view returns (uint32 timestamp, uint amount) {
+		require(exists(_tokenId));
+		TokenMetadata storage tm = metadata[_tokenId];
+		return (tm.timestamp, tm.amount);
 	}
 }
